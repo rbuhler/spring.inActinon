e682d014c9a4105a357249c197b5b690254f65fa
Rodrigo Buhler <rodrigo.buhler@ymail.com>
2017-02-09 19:21:13 -0200

    Page 44 - issue still open

--- src/main/com/springinaction/config/CDPlayerConfig.java
+++ src/main/com/springinaction/config/CDPlayerConfig.java
@@ -1,10 +1,15 @@
 package main.com.springinaction.config;
 
 import main.com.springinaction.soundsystem.CompactDisc;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import main.com.springinaction.soundsystem.SgtPeppers;
+import org.springframework.context.annotation.*;
 
 @Configuration
-@ComponentScan(basePackageClasses={CompactDisc.class})
+//@ComponentScan(basePackageClasses={CompactDisc.class})
 public class CDPlayerConfig {
+}
+
+@Bean(name="lonelyHeartsClubBand")
+public CompactDisc sgtPeppers() {
+    return new SgtPeppers();
 }
